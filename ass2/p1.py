dcf7ce3ffe34e8c83a9561d40b32857b5367f536
bottleimp <anakin.yan@gmail.com>
2014-05-29 00:37:02 +0800

    p2 half finished

--- ass2/p1.py
+++ ass2/p1.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 __author__ = 'anakin'
 
 import json
@@ -5,13 +7,10 @@ from count_cfg_freq import Counts
 
 
 class Tagger(Counts):
-    """
-    Store emissions.
-    """
     def __init__(self):
         Counts.__init__(self)
 
-        self.wc = {}
+        self.wc = defaultdict(int)
         self.rare_tag = '_RARE_'
         self.rare_threshold = 5
 
@@ -20,8 +19,6 @@ class Tagger(Counts):
         Count emission probabilities from a corpus file.
         """
         for (_, word), count in self.unary.iteritems():
-            if word not in self.wc:
-                self.wc.setdefault(word, 0)
             self.wc[word] += count
 
     def group_rare(self, word):
@@ -46,35 +43,80 @@ class Tagger(Counts):
             self.replace(tree[2])
         elif len(tree) == 2:
             # It is a unary rule.
-            word = tree[1]
-            if word not in self.wc or self.wc[word] < self.rare_threshold:
+            if self.is_rare_word(tree[1]):
                 tree[1] = self.rare_tag
 
-    def train(self, rare_file):
-        super(SimpleTagger, self).train(rare_file)
+    def is_rare_word(self, word):
+        return word not in self.wc or self.wc[word] < self.rare_threshold
 
-    def write_count(self, output):
-        """
-        Writes counts to the output file object.
-        """
-        for word in self.wc:
-            output.write("%s COUNTS %i\n" % (word, self.wc[word]))
 
-    def argmax_e(self, word):
-        def emission_iter(w):
-            for e in self.emission_counts.keys():
-                if isinstance(e, tuple) and e[0] == w:
-                    yield e
+class Parser(Tagger):
+    def __init__(self):
+        Tagger.__init__(self)
+        self.pi = defaultdict(float)
 
-        if word not in self.wc or self.wc[word] < self.rare_threshold:
-            word = self.group_rare(word)
-        return max(emission_iter(word), key=(lambda key: self.e_xy(key[0], key[1])))
+    def count_x(self, x):
+        return self.nonterm[x]
 
-    def y_star(self, word):
-        return self.argmax_e(word)
+    def count_x_w(self, x, w):
+        if self.is_rare_word(w):
+            if (x, self.rare_tag) not in self.unary:
+                return 0
+            return self.unary[(x, self.rare_tag)]
+        if (x, w) not in self.unary:
+            return 0
+        return self.unary[(x, w)]
 
-    def e_xy(self, x, y):
-        if x not in self.wc or self.wc[x] < self.rare_threshold:
-            x = self.group_rare(x)
-        return float(self.emission_counts[(x, y)]) / float(self.ngram_counts[0][(y,)])
+    def count_x_yy(self, x, y1, y2):
+        if (x, y1, y2) not in self.binary:
+            return 0
+        return self.binary[(x, y1, y2)]
+
+    def q_x_w(self, x, w):
+        return float(self.count_x_w(x, w)) / float(self.count_x(x))
+
+    def q_x_yy(self, x, y1, y2):
+        return float(self.count_x_yy(x, y1, y2)) / float(self.count_x(x))
+
+    def parser(self, sentence):
+        n = len(sentence)
+        '''
+        for i in xrange(0, n):
+            if self.is_rare_word(sentence[i]):
+                sentence[i] = self.rare_tag
+
+        print sentence
+                '''
+
+        w = [None]
+        w.extend(sentence)
+        pi = defaultdict(float)
+        bp = defaultdict(float)
+        for i in xrange(1, n+1):
+            for x in self.nonterm:
+                pi[(i, i, x)] = self.q_x_w(x, w[i])
+
+        for l in xrange(1, n):
+            for i in xrange(1, n):
+                j = i + l
+                for x in self.nonterm:
+                    t = [(y, z, s) for s in xrange(i, j) for (xp, y, z) in self.binary if xp == x]
+                    if t:
+                        bp[(i, j, x)] = max(t, key=lambda (y, z, s): self.q_x_yy(x, y, z) * pi[(i, s, y)] * pi[(s+1, j, z)])
+                        (y, z, s) = bp[(i, j, x)]
+                        pi[(i, j, x)] = self.q_x_yy(x, y, z) * pi[(i, s, y)] * pi[(s+1, j, z)]
+
+        return self.build_tree(1, n, 'SBARQ', w, bp)
+
+    def build_tree(self, start, end, tag, words, bp):
+        tree = list()
+        tree.append(tag)
+        if start == end:
+            tree.append(words[start])
+        else:
+            y1, y2, mid = bp[(start, end, tag)]
+            tree.append(self.build_tree(start, mid, y1, words, bp))
+            tree.append(self.build_tree(mid+1, end, y2, words, bp))
+
+        return tree
 
